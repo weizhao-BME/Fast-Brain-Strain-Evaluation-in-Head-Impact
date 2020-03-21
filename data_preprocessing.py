ce1c697eca988f216a4db688eb3db32a904c7f32
xzhan96-stf <59583341+xzhan96-stf@users.noreply.github.com>
2020-03-21 15:42:50 -0700

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,38 +3,64 @@ from scipy.io import loadmat
 from sklearn.decomposition import PCA
 from sklearn.metrics import r2_score
 
+# indexes to delete
+id_delete = [83,84,85,86,87,88,89,270] + list(range(342,353))
 
-# load original data (not PCAed) of football dataset
-def loadOriginalData():
+# load original AF_preprocess data and 95 percentile data
+def loadAFPreprocessData():
+    AF_X_dict = loadmat('preprocess_AF_X.mat')
+    AF_X = AF_X_dict['data']
+    AF_Y__95_Dict = loadmat('AF_Y_95.mat')
+    AF_Y_95 = AF_Y__95_Dict["AF_Y_95"]
+    return AF_X, AF_Y_95
+
+# load original AF data
+def loadAFData():
+    AF_X_Dict = loadmat('preprocess_AF_X.mat')
+    AF_X = AF_X_Dict["data"]
+    AF_Y_Dict = loadmat('AF_Y.mat')
+    AF_Y = AF_Y_Dict["AF_Y"]
+    return AF_X, AF_Y
+
+# load original AF and 95 percentile data
+def loadAF95Data():
     AF_X_Dict = loadmat('AF_X.mat')
     AF_X = AF_X_Dict["AF_X"]
-    AF_Y_Dict = loadmat('football_Y.mat')
-    AF_Y = AF_Y_Dict["label_data"]
-    return AF_X, AF_Y
+    AF_Y__95_Dict = loadmat('AF_Y_95.mat')
+    AF_Y_95 = AF_Y__95_Dict["AF_Y_95"]
+    return AF_X, AF_Y_95
+
+# load original MMA data
+def loadMMAData():
+    MMA_X_Dict = loadmat('MMA_X_preprocess.mat')
+    MMA_X = MMA_X_Dict["MMA_X"]
+    MMA_Y_Dict = loadmat('MMA_Y.mat')
+    MMA_Y = MMA_Y_Dict["label_data"]
+    return MMA_X, MMA_Y
 
 # Data index: create split index (random function), get num of samples and trainSet ratio
 # returns a partition index
-def getIndex(num, ratio):
-    np.random.seed(9001)
+def getIndex(num, ratio, seed=9001):
+    np.random.seed(seed)
     index = np.random.permutation(np.arange(num))
     trainIndex = index[:int(num*ratio)]
     testIndex = index[int(num*ratio):]
     return trainIndex, testIndex
 
-# Data split: takes two ndarrays: X, Y and ratio of training set. Returns (X_train, X_test, Y_train, Y_test)
-def dataSplit(X, Y, ratio = 0.8):
-    # Assume X.shape = (time_span, channel, sample), Y.shape = (sample, 4124)
+# Data split: takes two ndarrays: X, Y and ratio of training set.
+# Returns (X_train, X_test, Y_train, Y_test)
+def dataSplit(X, Y, ratio = 0.8, seed=9001):
+    # Assume X.shape = (N, timespan, channels), Y.shape = (sample, 4124) or (sample, 1)
     # the first dimension of Y is sample size, so as the third dimension of X
-    trainIndex, testIndex = getIndex(Y.shape[0], ratio)
-    # transpose X.shape to be (sample, channel, time_span)
-    X = X.transpose((2,1,0))
+    trainIndex, testIndex = getIndex(Y.shape[0], ratio, seed)
     X_train = X[trainIndex,:,:]
     Y_train = Y[trainIndex,:]
     X_test = X[testIndex,:,:]
     Y_test = Y[testIndex,:]
     return X_train, X_test, Y_train, Y_test
 
-# standardize the first dimension(sample) of Y_train, return  Y_train_std, element_mean, element_std
+# standardize the first dimension(sample) of Y_train,
+# return: Y_train_std, element_mean, element_std
 def standardizeYData(Y_train):
     element_mean = np.mean(Y_train, axis=0, keepdims=True)
     element_std = np.std(Y_train, axis=0, keepdims=True)
@@ -70,7 +96,8 @@ def reconstructYFromPCA(Y, pca_model):
 # reconstruct Y from PCA and standardization
 def reconstructY(Y, mean, std, pca_model):
     Y_invert_pca = reconstructYFromPCA(Y, pca_model)
-    return reconstructYFromStd(Y_invert_pca, mean, std)
+    #print(Y_invert_pca[0,:])
+    return reconstructYFromStd(Y_invert_pca, mean, std), Y_invert_pca
 
 # Augmentation of data: by copying data three times and add
 # 1.nothing 2. noise ~ N(0,0.01STD)^2) 3. noise ~ N(0,0.02STD)^2)
@@ -91,6 +118,5 @@ def train_augment(trainX, trainY):
 
 # flatten X to be the shape (sample, channel * ms) (for cnn model)
 def flatten_X(X):
-    assert X.shape[1] == 6, "X channel number error"
-    assert X.shape[2] == 100, "X timespan length error"
     return X.reshape(X.shape[0],X.shape[1]*X.shape[2])
+
