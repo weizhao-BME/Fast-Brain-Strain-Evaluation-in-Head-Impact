d644511f7975de7f3fe903164cdee179f82326ac
xzhan96-stf <59583341+xzhan96-stf@users.noreply.github.com>
2020-03-21 15:45:52 -0700

    Update model_fit.py

--- model_fit.py
+++ model_fit.py
@@ -11,7 +11,7 @@ from numpy.random import seed
 def modelFit(model, X_train, Y_train, epoch=50, analyze=True):
     seed(9001)
     train_start = time.time()
-    history = model.fit(X_train, Y_train, epochs=epoch, batch_size=64, validation_split=0.1, verbose=0, shuffle=False)
+    history = model.fit(X_train, Y_train, epochs=epoch, batch_size=64, verbose=0, shuffle=False)
     train_finish = time.time()
     if analyze:
         print("Spend {} sec training model".format(round(train_finish-train_start,1)))
@@ -20,4 +20,4 @@ def modelFit(model, X_train, Y_train, epoch=50, analyze=True):
         val_loss, = plt.plot(history.history['val_loss'])
         plt.legend([loss, val_loss],['loss','Val_loss'])
         plt.show()
-    return model
+    return model, history
